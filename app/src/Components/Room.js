30194900778c5ab8f3b2c8d1362085893ce5a145
Choi Kyuhyeon <bluejet1104@gmail.com>
2020-01-25 15:57:24 +0900

    Room page initial commit

---
+++ app/src/Components/Room.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Room extends Component {
+  render() {
+    return (
+      <div className={"roomContainer"}>
+        <div className={"titleBar"}></div>
+      </div>
+    );
+  }
+}
